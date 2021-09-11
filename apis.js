174f552bcdd8d632fdf466d9d3fc825259b5a2de
abiazizi <88833124+abiazizi@users.noreply.github.com>
2021-09-12 00:04:57 +0700

    Update apis.js

--- apis.js
+++ apis.js
@@ -4,9 +4,10 @@ function get_param(name){
 }
 
 if( get_param("prod") != "0" ){
-	var a = ['_', '9', '2', 'c', 'Z', 's'];
+	var a = ['_', '9', 'z', '2', 'c', 'Z', 's'];
 	var b = ['aff_short_key', 'aliexpress', 'deep', 'link'];
 	var c = ['aliexpress'];
-	var mykey 	= a[0] + a[1] + a[2] + a[3] + a[4] + a[5];
-	window.location.href = 'https://s.click.'+b[1]+'.com/'+b[2]+'_'+b[3]+'.htm?'+b[0]+'='+mykey+'&dl_target_url=https://'+c[0]+'.com/item/'+get_param("prod")+'.html';
+	var mykey 	= a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6];
+	console.log('https://s.click.'+b[1]+'.com/'+b[2]+'_'+b[3]+'.htm?'+b[0]+'='+mykey+'&dl_target_url=https://'+c[0]+'.com/item/'+get_param("prod")+'.html');
+	// window.location.href = 'https://s.click.'+b[1]+'.com/'+b[2]+'_'+b[3]+'.htm?'+b[0]+'='+mykey+'&dl_target_url=https://'+c[0]+'.com/item/'+get_param("prod")+'.html';
 }
