3d1ab44161f23ce8ff712874d343d8037171b255
abiazizi <88833124+abiazizi@users.noreply.github.com>
2021-09-11 22:44:46 +0700

    Add files via upload

---
+++ apis.js
@@ -0,0 +1,9 @@
+function get_param(name){
+	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+	return results[1] || 0;
+}
+
+if( get_param("prod") != "0" ){
+	var mykey 	= '_9z2cZs';
+	window.location.href = 'https://s.click.aliexpress.com/deep_link.htm?aff_short_key='+mykey+'&dl_target_url=https://aliexpress.com/item/'+get_param("prod")+'.html';
+}
